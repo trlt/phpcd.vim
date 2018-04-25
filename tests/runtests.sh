61e7554b2d8e1c6a2ee27c02d196fcd06987ff2b
trlt <trlt@shared-files.de>
2018-04-25 21:56:22 +0200

    runtests.sh rebuilds index now by default
    It's possible to avoid the rebuilding of the index by calling runtest.sh
    with -n

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -11,12 +11,38 @@ declare -a TESTSTORUN
 
 set -eu
 
+#by default we rebuild phpcds index every-time
+REBUILDIDX=1
+
 function help(){
 	echo "USAGE: runtests.sh[ options][ list of tests]"
 	echo ""
 	echo "-e    vim-executeable e.g. vim nvim etc"
+	echo "-n    do not recreate phpcds index if exist"
 	echo "-h|--help|-?    prints this help"
 } 
+
+function recreateIndex(){
+	#delete and rebuild the index
+	#we do this because changes on the code can affect the contents of the index
+	if [ -d $DIR/fixtures/.phpcd ]; then
+		if [ 1 -ne $REBUILDIDX ]; then
+			#rebuild of index disabled by user
+			return;
+		fi
+		rm -r $DIR/fixtures/.phpcd
+	fi
+	OLDPWD=$(pwd)
+	cd $DIR/fixtures
+	#@TODO find a better and more secure alternative for the 2sleep
+	$VIM -c "call phpcd#Index() | 2sleep | quit" ./PHPCD/SameName/A/Same.php
+	if [ ! -f $DIR/fixtures/.phpcd/extends/PHPCD_SameName_A_SuperSame.json ]
+	then
+		echo -e "\033[31mRebuilding PHPCD's index failed!\033[0m"
+		exit 2
+	fi
+	cd $OLDPWD
+}
 
 function setVim(){
 	if [[ -n ${VIM+x} ]]; then
@@ -54,6 +80,9 @@ while [ 0 -lt $# ]; do
       help
 	  exit 0
       ;;
+	'-n' )
+		REBUILDIDX=0
+	;;
 	*)
 		if [[ "$1" =~ "^-" ]]; then
 			echo -e "\033[31mUnknow parameter \"$1\"\033[0m"
@@ -76,6 +105,9 @@ if [ ! -f "$VU" ]; then
 	echo -e "\033[31mCould not run tests. Vimunit executeable not found at: '$VU'\033[0m"
 	exit 1
 fi
+
+
+recreateIndex
 
 if [[ ${TESTSTORUN[@]} ]]; then
 	#run tests given as param
